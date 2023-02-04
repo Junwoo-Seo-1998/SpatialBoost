0319ec02dce2cb0a3ba05ab9b6aca37bb35940fa
Junwoo-Seo <junwoo.seo.1998@gmail.com>
2023-02-03 22:23:45 -0800

    update header and readme

--- RenderingEngine/Core/Layer/SkyboxRenderLayer.cpp
+++ RenderingEngine/Core/Layer/SkyboxRenderLayer.cpp
@@ -1,3 +1,12 @@
+/* Start Header -------------------------------------------------------
+Copyright (C) 2022 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior written
+consent of DigiPen Institute of Technology is prohibited.
+Language: C++ MSVC(2022)
+Platform: MSVC(2022), GPU needs to support OpenGL 4.6.0, Windows11(x64)
+Project: junwoo.seo_cs350_1
+Author: Junwoo Seo, junwoo.seo, 0055213
+End Header --------------------------------------------------------*/
 #include "SkyboxRenderLayer.h"
 
 #include "imgui.h"
